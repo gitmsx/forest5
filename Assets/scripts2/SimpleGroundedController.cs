ad8c9c5bfd638c798494b9b110fb8a757edd8e86
gitmsx <gitmsx@mail.ru>
2022-11-05 18:24:55 +0300

    Saved on 05.11.2022-18:24:55,52

---
+++ Assets/scripts2/SimpleGroundedController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleGroundedController : MonoBehaviour
+{
+    public float JumpImpulse = 7f;
+    public float SideSpeed = 2f;
+
+    public ContactFilter2D ContactFilter;
+
+    private Rigidbody2D m_Rigidbody;
+    private bool m_ShouldJump;
+    private float m_SideSpeed;
+
+    // We can check to see if there are any contacts given our contact filter
+    // which can be set to a specific layer and normal angle.
+    public bool IsGrounded => m_Rigidbody.IsTouching(ContactFilter);
+
+    private bool FaceRight = true;
+
+
+    private void flip()
+    {
+        FaceRight = !FaceRight;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.x);
+    }
+
+    private bool LeftArrow = false;
+    private bool RightArrow = false;
+
+
+
+
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+
+    void Update()
+    {
+        // Set jump/
+        if (Input.GetKeyDown(KeyCode.Space))
+            m_ShouldJump = true;
+
+
+        LeftArrow = Input.GetKey(KeyCode.LeftArrow);
+        RightArrow = Input.GetKey(KeyCode.RightArrow);
+        
+        m_SideSpeed = ( LeftArrow  ? -SideSpeed : 0f) + (RightArrow ? SideSpeed : 0f);
+
+        
+
+                if (LeftArrow  && (FaceRight == true))
+                {
+                    flip();
+                }
+                else if (RightArrow  && (FaceRight != true))
+                {
+                    flip();
+                }
+    }
+
+
+   
+
+
+    void FixedUpdate()
+    {
+        // Handle jump.
+        // NOTE: If instructed to jump, we'll check if we're grounded.
+        if (m_ShouldJump && IsGrounded)
+            m_Rigidbody.AddForce(Vector2.up * JumpImpulse, ForceMode2D.Impulse);
+
+        // Set sideways velocity.
+        m_Rigidbody.velocity = new Vector2(m_SideSpeed, m_Rigidbody.velocity.y);
+
+
+
+
+
+
+        // Reset movement.
+        m_ShouldJump = false;
+        m_SideSpeed = 0f;
+    }
+}
